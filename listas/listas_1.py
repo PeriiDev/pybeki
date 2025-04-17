8ce03cb9e835fe03af9811ec61e1df23fc0849e8
navarrocpedroa <navarroc.pedroa@gmail.com>
2025-04-17 16:29:19 +0200

    first commit

---
+++ listas/listas_1.py
@@ -0,0 +1,14 @@
+"""
+Ejercicio 1: Crear y mostrar una lista
+"""
+
+# Creamos una lista de frutas
+frutas = ["manzana", "banana", "naranja"]
+# Mostramos la lista
+print(frutas)
+
+
+"""
+Ejercicio propuesto: Crea una lista con tres nombres de tus amigos y muéstrala en pantalla.
+"""
+
